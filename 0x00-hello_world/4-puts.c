f1326ad649bb74a577c86975fbb9692c5e04a8c0
Ibeksconde <79115797+Ibeksconde@users.noreply.github.com>
2022-03-10 23:17:15 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,14 +5,10 @@
 * puts -print declared char string to std output
 *
 * Return: Always 0 (Success)
-*/	
-int main(void)	
-{	
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}/
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
