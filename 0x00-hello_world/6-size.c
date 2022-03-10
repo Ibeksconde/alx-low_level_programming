46f037f7c71c3ce6db07c973ad3f8bbb70429856
Ibeksconde <ibeksconde@gmail.com>
2022-03-10 15:19:38 -0800

    task 6 correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
 * main - Entry point
 *
-* sizeof - determines variable size by data type prints to stdio
+* sizeof - determines vriable size by data type prints to stdio
 *
 * Return: Always 0 (Success)
 */
@@ -12,6 +12,6 @@ printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
+printf("Size of a float: %d bytes(s)\n", sizeof(long long int));
 return (0);
 }
