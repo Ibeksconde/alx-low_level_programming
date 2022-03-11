5ff12c1451c12dc36e156993fab7f150d23affd1
Ibeksconde <79115797+Ibeksconde@users.noreply.github.com>
2022-03-11 07:16:14 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-/**
-* main - Entry point
-*
-* printf - function to which variables print to std output
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
-}
+#include<<stdio.h>                                                   
+/**                                                                  
+*main - Entry point                                                  
+*                                                                    
+*printf - function to which variables print to std output            
+*                                                                    
+*Return: Always 0 (success)                                          
+*/                                                                   
+int main(void)                                                       
+{                                                                    
+        printf("with proper grammar, but the outcome is a piece of ar
+t,\n");                                                              
+        return (0);                                                  
+}                                                                    
