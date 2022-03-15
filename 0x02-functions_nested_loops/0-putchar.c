c8fb2410b21958566151389bd8704806c0e7c1db
Ibeksconde <ibeksconde@gmail.com>
2022-03-15 14:59:07 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,32 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
+* main - Entry block
+* Description: writes the name of the file.
 *
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* Return: 0
 */
 
-int _putchar(char c)
+int main(void)
 
 {
 
-		return (write(1, &c, 1));
+	 char c[8] = "_putchar";
+
+	  int i = 0;
+
+	   while (i < 8)
+
+		     {
+
+			         _putchar(c[i]);
+
+				     i++;
+
+		     }
+
+	    _putchar('\n);
+
+	     return (0);
 
 }
