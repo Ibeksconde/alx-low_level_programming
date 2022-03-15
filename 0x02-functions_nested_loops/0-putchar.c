84b678294bed256152633083b23278f34a6ec2a5
Ibeksconde <79115797+Ibeksconde@users.noreply.github.com>
2022-03-15 23:15:49 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,25 +8,15 @@
 */
 
 int main(void)
-
 {
-
-	 char c[8] = "_putchar";
-
-	  int i = 0;
-
-	   while (i < 8)
-
-		     {
-
-			         _putchar(c[i]);
-
-				     i++;
-
-		     }
-
-	    _putchar('\n');
-
-	     return (0);
-
+	char c[8] = "_putchar";
+	int i = 0;
+	while (i < 8)
+		{
+			_putchar(c[i]);
+			i++;
+		}
+	_putchar('\n');
+	return (0);
+	
 }
